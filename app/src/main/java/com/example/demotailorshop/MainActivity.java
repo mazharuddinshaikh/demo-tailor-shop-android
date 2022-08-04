a53f2bafc50737f5e5fe4162bfce0e37b9798229
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2022-08-04 21:06:16 +0530

    test commit removed extra space

--- app/src/main/java/com/example/demotailorshop/MainActivity.java
+++ app/src/main/java/com/example/demotailorshop/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Intent intent = getIntent();
         String authenticationFailedMessage = null;
         if (intent != null) {
