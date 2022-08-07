5f7492c318059e53434c34f9e9127c7c5c2bc1d3
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2022-08-07 17:49:39 +0530

    increase http request timeout to 10 minutes

--- app/src/main/java/com/example/demotailorshop/api/DtsApiFactory.java
+++ app/src/main/java/com/example/demotailorshop/api/DtsApiFactory.java
@@ -1,19 +1,29 @@
 package com.example.demotailorshop.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 final public class DtsApiFactory {
     private static final String BASE_URL = "http://192.168.227.119:8081/";
     private static Retrofit retrofit;
-//authTkoen "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXpoYXIiLCJpYXQiOjE2Mzk4MjM0OTgsImlzcyI6ImR0cy10YWlsb3Itc2hvcCJ9.hbRHvYk9EMi9UrYhFFXrMjV2yzQa4fj1kB0nRyMUXVI"
+
+    //authTkoen "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXpoYXIiLCJpYXQiOjE2Mzk4MjM0OTgsImlzcyI6ImR0cy10YWlsb3Itc2hvcCJ9.hbRHvYk9EMi9UrYhFFXrMjV2yzQa4fj1kB0nRyMUXVI"
     private DtsApiFactory() {
     }
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.MINUTES)
+                    .readTimeout(10, TimeUnit.MINUTES)
+                    .writeTimeout(10, TimeUnit.MINUTES)
+                    .build();
             retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
                     .addConverterFactory(JacksonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
